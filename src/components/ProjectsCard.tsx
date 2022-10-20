4f79c19f1186534ba4401e8c105aa2dc15008f0a
Gaspi <gasparelpi@gmail.com>
2022-10-21 00:03:37 +0200

    fixing modals

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -35,46 +35,49 @@ function ProjectsCard({
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose} size="2xl">
+      <Modal isOpen={isOpen} onClose={onClose} size="4xl">
         <ModalOverlay />
-        <ModalContent my="auto">
-          <ModalHeader>
-            <Text color="secondary" fontSize="4xl" fontFamily={theme.fonts.secondary}>
+        <ModalContent m="auto" py="4">
+          <ModalHeader pb="0">
+            <Text color="secondary" textAlign="center" fontSize="4xl" fontFamily={theme.fonts.secondary}>
               {title}
             </Text>
           </ModalHeader>
           <ModalCloseButton _hover={{ bg: "none" }} />
-          <ModalBody color="primary" display="flex" flexDir="column" gap={4}>
-            <Text fontSize="xl" fontFamily={theme.fonts.primary}>
+          <ModalBody color="primary" display="flex" flexDir="row" gap={4}>
+            <Flex flexDir="column" justify="space-around" gap="4">
+            <Text fontSize="xl" fontWeight="bold" fontFamily={theme.fonts.primary}>
               {description}
             </Text>
             <Text fontSize="xl" fontFamily={theme.fonts.primary}>
               {`Stack: ${stack}`}
             </Text>
-            <Flex>
+            <Flex align="center">
               {github && (
                 <>
-                  <Text mr="3" fontSize="xl" fontFamily={theme.fonts.primary}>
-                    Repositorio:{" "}
-                  </Text>
-                  <Link href={github}>
+                  <Link href={github} display="flex" alignItems="center" _hover={{textDecoration: "none"}} isExternal>
                     <Icon as={AiOutlineGithub} fill="secondary" fontSize="4xl" />
+                  <Text mr="3" fontSize="lg" fontFamily={theme.fonts.primary}>
+                    : {github}
+                  </Text>
                   </Link>
                 </>
               )}
             </Flex>
-            <Flex>
+            <Flex align="center">
               {web && (
                 <>
-                  <Text mr="3" fontSize="xl" fontFamily={theme.fonts.primary}>
-                    Web:{" "}
-                  </Text>
-                  <Link href={web}>
+                  <Link href={web} display="flex" alignItems="center" _hover={{textDecoration: "none"}} isExternal>
                     <Icon as={AiOutlineGlobal} fill="secondary" fontSize="4xl" />
+                  <Text mr="3" fontSize="lg" fontFamily={theme.fonts.primary}>
+                    : {web}
+                  </Text>
                   </Link>
                 </>
               )}
             </Flex>
+            </Flex>
+            <Image alt="project image" src={src} w="300px" h="300px" display={{base: "none", md: "block"}} />
           </ModalBody>
         </ModalContent>
       </Modal>
@@ -100,13 +103,13 @@ function ProjectsCard({
             objectFit="fill"
             alt={title}
           />
-          <Flex p="0.5rem" gap="0.5rem" flexDir="column">
-            
+          <Flex p="0.5rem" gap="0.5rem" flexDir="column">      
               <Text
                 fontFamily={theme.fonts.secondary}
                 textAlign="center"
                 fontSize="2xl"
                 color="#505050"
+                mt="2"
               >
                 {title}
               </Text>
